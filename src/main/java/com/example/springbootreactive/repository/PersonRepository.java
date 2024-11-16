89c4fe3a0d9f931ddd2e6ee2fa7b7d78c43133a4
uicmu <mubin41@student.sust.edu>
2024-11-16 15:00:37 +0600

    Testing repository functionalities

--- src/main/java/com/example/springbootreactive/repository/PersonRepository.java
+++ src/main/java/com/example/springbootreactive/repository/PersonRepository.java
@@ -1,7 +1,28 @@
 package com.example.springbootreactive.repository;
 
 import com.example.springbootreactive.entity.Person;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface PersonRepository extends R2dbcRepository<Person, Long> {
+    /**
+     *  Query Methods
+     *  Find Methods
+     */
+
+    Mono<Person>findByName(String name);
+
+    Flux<Person>findByAddress(String address);
+
+    Flux<Person>findByNameAndAddress(String name, String address);
+
+    /**
+     * the name will come from string name
+     */
+    @Query("SELECT * FROM person WHERE name=:name")
+    Flux<Person> getAllPersonsByName(String name);
+
+
 }
